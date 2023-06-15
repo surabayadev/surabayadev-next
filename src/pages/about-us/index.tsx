be151da1c13e7436f1059414bf57c46fe5d7033c
ahmadsoni <ahmadshonhaji476@gmail.com>
2023-06-15 12:06:50 +0700

    :update minor: about us

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 import Layout from '@/components/layout/Layout';
 
+import galery from './galery';
 import teams from './teams';
-
 // import { useState } from "react";
 
 export default function AboutUs() {
@@ -13,7 +13,7 @@ export default function AboutUs() {
     <Layout>
 
       <section className="bg-gradient-to-b from-gray-100 to-white">
-        <div className="absolute inset-x-0 -bottom-14 -top-48 overflow-hidden bg-green-50">
+        <div className="absolute inset-x-0 -bottom-80 lg:-bottom-14 -top-48 overflow-hidden bg-green-50">
           <div className="absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-white"></div>
           <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-t from-white"></div>
         </div>
@@ -27,69 +27,65 @@ export default function AboutUs() {
 
               <p className="font-normal text-gray-500 text-base md:text-lg lg:text-xl mb-16">
                 SurabayaDev merupakan komunitas teknologi yang dibentuk sejak
-                tahun 2010 <br /> dan merupakan wadah untuk belajar dan kolaborasi
+                tahun 2014 <br /> dan merupakan wadah untuk belajar dan kolaborasi
                 di Kota Surabaya
               </p>
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-2 mb-12 lg:mb-24">
+            <div className="grid grid-cols-1 lg:grid-cols-3 mb-12 lg:mb-24">
 
               <div className="card">
                 <div className="text-center mb-20 lg:mb-0">
                   <div className="flex justify-center mb-5">
-                    <svg
-                      width="24"
-                      height="25"
-                      viewBox="0 0 24 25"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M8.59 14.0726L15.42 18.0526M15.41 7.07262L8.59 11.0526M21 5.56262C21 7.21948 19.6569 8.56262 18 8.56262C16.3431 8.56262 15 7.21948 15 5.56262C15 3.90577 16.3431 2.56262 18 2.56262C19.6569 2.56262 21 3.90577 21 5.56262ZM9 12.5626C9 14.2195 7.65685 15.5626 6 15.5626C4.34315 15.5626 3 14.2195 3 12.5626C3 10.9058 4.34315 9.56262 6 9.56262C7.65685 9.56262 9 10.9058 9 12.5626ZM21 19.5626C21 21.2195 19.6569 22.5626 18 22.5626C16.3431 22.5626 15 21.2195 15 19.5626C15 17.9058 16.3431 16.5626 18 16.5626C19.6569 16.5626 21 17.9058 21 19.5626Z"
-                        stroke="#3B82F6"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"  stroke="#3B82F6"
                         strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"></path>
+                        />
                     </svg>
                   </div>
 
                   <h3 className="font-medium text-gray-900 text-lg lg:text-xl mb-2">
-                    Social Media Marketing
+                    Memperluas Koneksi
                   </h3>
 
-                  <p className="font-normal text-gray-500 text-sm lg:text-base">
-                    Lorem ipsum dolor sit amet, consectetur <br /> adipiscing
-                    elit, sed do eiusmod tempor incididunt <br /> ut labore et
-                    dolore magna aliqua.
+                  <p className="font-normal text-gray-500 text-sm lg:text-base px-12">
+                   Memperluas relasi melalui komunitas untuk menjalin kebermanfaatan.
                   </p>
                 </div>
               </div>
 
               <div className="card">
-                <div className="text-center">
+                <div className="text-center mb-20 lg:mb-0">
                   <div className="flex justify-center mb-5">
-                    <svg
-                      width="24"
-                      height="25"
-                      viewBox="0 0 24 25"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M14 2.56262H6C5.46957 2.56262 4.96086 2.77334 4.58579 3.14841C4.21071 3.52348 4 4.03219 4 4.56262V20.5626C4 21.0931 4.21071 21.6018 4.58579 21.9768C4.96086 22.3519 5.46957 22.5626 6 22.5626H18C18.5304 22.5626 19.0391 22.3519 19.4142 21.9768C19.7893 21.6018 20 21.0931 20 20.5626V8.56262M14 2.56262L20 8.56262M14 2.56262V8.56262H20M16 13.5626H8M16 17.5626H8M10 9.56262H8"
-                        stroke="#3B82F6"
-                        strokeWidth="1.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"></path>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" stroke="#3B82F6" />
                     </svg>
+
                   </div>
 
                   <h3 className="font-medium text-gray-900 text-lg lg:text-xl mb-2">
-                    Content Writing
+                    Berbagi Ilmu
                   </h3>
 
-                  <p className="font-normal text-gray-500 text-sm lg:text-base">
-                    Lorem ipsum dolor sit amet, consectetur <br /> adipiscing
-                    elit, sed do eiusmod tempor incididunt <br /> ut labore et
-                    dolore magna aliqua.
+                  <p className="font-normal text-gray-500 text-sm lg:text-base px-10">
+                   Menjadi wadah yang tepat untuk bisa saling berbagi ilmu, pengalaman maupun wawasantentang update teknologi.
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="text-center mb-20 lg:mb-0">
+                  <div className="flex justify-center mb-5">
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"  stroke="#3B82F6"/>
+                  </svg>
+                  </div>
+
+                  <h3 className="font-medium text-gray-900 text-lg lg:text-xl mb-2">
+                   Wadah Belajar
+                  </h3>
+
+                  <p className="font-normal text-gray-500 text-sm lg:text-base px-10">
+                   Menjadi wadah belajar pegiat IT untuk bisa terus update mengenai tech industry saat ini.
                   </p>
                 </div>
               </div>
@@ -99,9 +95,10 @@ export default function AboutUs() {
           {/* Gallery */}
           <section className="relative">
             <div className="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3">
-              <div className="w-full rounded hover:shadow-2xl">
+                {galery.map((item) => (
+              <div className="w-full rounded hover:shadow-2xl flex items-center" key={item.details}>
                 <Image
-                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                  src={item.image}
                   alt="image"
                   width={100}
                   height={100}
@@ -109,56 +106,7 @@ export default function AboutUs() {
                   style={{ width: '100%', height: 'auto' }}
                 />
               </div>
-              <div className="w-full rounded hover:opacity-50">
-                <Image
-                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                  alt="image"
-                  width={100}
-                  height={100}
-                  sizes="100vw"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
-              <div className="w-full rounded hover:shadow-2xl">
-                <Image
-                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                  alt="image"
-                  width={100}
-                  height={100}
-                  sizes="100vw"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
-              <div className="w-full rounded hover:shadow-2xl">
-                <Image
-                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                  alt="image"
-                  width={100}
-                  height={100}
-                  sizes="100vw"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
-              <div className="w-full rounded hover:shadow-2xl">
-                <Image
-                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                  alt="image"
-                  width={100}
-                  height={100}
-                  sizes="100vw"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
-              <div className="w-full rounded hover:shadow-2xl">
-                <Image
-                  src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-                  alt="image"
-                  width={100}
-                  height={100}
-                  sizes="100vw"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </div>
+                ))}
             </div>
           </section>
 
